a640f6aa5f0efad2bebdcd05e7fc76f3c171c30f
Prince450 <princetonabdulsalam25@gmail.com>
2025-02-15 17:51:33 -0500

    final

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable vpc_cidr_blocks {}
+variable subnet_cidr_blocks {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable image_name {}
+variable public_key_location {}
